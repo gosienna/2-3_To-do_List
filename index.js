7b6df453e46e706ed8cfc989fedf112745a83f2d
gosienna <gosienna@gmail.com>
2021-10-10 23:06:30 +0800

    add edit delete functionality

--- index.js
+++ index.js
@@ -49,8 +49,25 @@ app.get('/todos/:id', function(req, res){
         .then(function(todo){res.render('detail',{todo})})
         .catch(function(error){console.log(error)})
 })
-
-
+////router to edit specific item
+app.get('/todos/:id/edit', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+      .lean()
+      .then((todo) => res.render('edit', { todo }))
+      .catch(error => console.log(error))
+  })
+app.post('/todos/:id/edit', (req, res) => {
+    const id = req.params.id
+    const name = req.body.name
+    return Todo.findById(id)
+        .then(todo => {
+        todo.name = name
+        return todo.save()
+        })
+        .then(()=> res.redirect(`/todos/${id}`))
+        .catch(error => console.log(error))
+})
 
 ////router to add new item
 app.post('/todos',function( req , res){
@@ -59,7 +76,14 @@ app.post('/todos',function( req , res){
         .then(function(){res.redirect('/')})
         .catch(function(error){console.log(error)})
 })
-
+/////router to delete an item
+app.post('/todos/:id/delete', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+      .then(todo => todo.remove())
+      .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
+  })
 
 
 //set port 3000
